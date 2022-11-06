b308207781b21528312aa850b3fdf5e98b067292
Bishal Rai <bishal6193@gmail.com>
2022-11-07 04:51:02 +0545

    done styling footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+
+//API address has provided to fetch the data
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
 export default function Home() {
   return (
     <div>This is Home</div>
   )
 }
+
